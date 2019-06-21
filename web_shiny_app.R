9a010f92f5538ae3e36ae38854f9208d87f85a5d
Vincent Lin <vl45@duke.edu>
2019-06-21 16:19:37 -0400

    web scraping started

---
+++ web_shiny_app.R
@@ -0,0 +1,48 @@
+library(rvest)
+library(tidyverse)
+library(stringr)
+
+all_artist_lyrics <- function(artist_name){
+  artist <- artist_name %>%
+    tolower()
+  artist <- gsub(" ", "-", artist)
+
+  
+  url_start <- "http://www.songlyrics.com/"
+  full_url <- paste0(url_start, artist, "-lyrics/")
+
+  
+ song_nodes <- full_url %>%
+   read_html() %>%
+   html_nodes("#colone-container .tracklist a")
+
+ 
+ song_links <- html_attr(song_nodes, name="href")
+
+ 
+ song_titles <- song_nodes %>%
+   html_text()
+
+ 
+ lyrics <- tibble()
+ for (i in 1:2){
+   message(str_c("Scraping Song ", i, " of ", length(song_links)))
+   
+   #scrape the song lyrics
+   lyrics_scraped <- song_links[i] %>%
+     read_html %>%
+     html_nodes("#songLyricsDiv") %>%
+     html_text()
+   
+   # format song for dataframe
+   song_title <- song_titles[i] %>%
+     str_to_lower() %>%
+     gsub("[^[:alnum:] ]", "", .) %>%
+     gsub("[[:space:]]", "_", .)
+   
+   lyrics <- rbind(lyrics, tibble(text=lyrics_scraped, artist=artist_name, song=song_title))
+ }
+ return(lyrics)
+}
+
+all_artist_lyrics("Katy Perry")
